1ba1722ff97c02e25bd99caa932a293eaea1977c
Dmitrij <49795742+DmitrijVC@users.noreply.github.com>
2020-12-22 12:50:18 +0100

    Update mod.rs

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -117,13 +117,8 @@ impl<'a> ClientBuilder<'a> {
     /// Sets a token for the bot. If the token is not prefixed "Bot ",
     /// this method will automatically do so.
     pub fn token(mut self, token: impl AsRef<str>) -> Self {
-        let token = token.as_ref().trim();
 
-        let token = if token.starts_with("Bot ") {
-            token.to_string()
-        } else {
-            format!("Bot {}", token)
-        };
+        let token = token.as_ref().trim().to_string();
 
         self.http = Some(Http::new_with_token(&token));
 
